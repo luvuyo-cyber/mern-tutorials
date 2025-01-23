06930d5caaa58d06a1377add8795641d17b958ce
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-01-23 03:12:54 +0200

    Add backend setup with Express, MongoDB, and book model

---
+++ Bookstore-MERN-Stack/backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://fisokuhlelavuyo97:N5ocr7iuxzBQhHOs@cluster0.byeyv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
