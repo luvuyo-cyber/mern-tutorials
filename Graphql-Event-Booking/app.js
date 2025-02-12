ab33d6ace48d0ce723c799f65419db62a8105907
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-02-12 12:44:59 +0200

    Add GraphQL support for event management; implement schema and resolvers

--- Graphql-Event-Booking/app.js
+++ Graphql-Event-Booking/app.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 app.use(bodyParser.json()); //to process incoming json bodies
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery  {
+                events: [String!]!
+        }
 
+        type RootMutation   {
+                createEvent(name:   String) : String
+        }
+
+        schema  {
+                query:  RootQuery
+                mutation:   RootMutation
+        }
+        `),
+    rootValue: {
+      events: () => {
+        return [
+          "Night out with the Homies",
+          "Watching Harry Potter",
+          "Get lucky",
+        ];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 app.get("/", (req, res, next) => {
   res.send("Hello World");
 });
