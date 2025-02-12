baa41a3f1faefbc9609e6289c1714da321e51856
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-02-12 13:49:55 +0200

    Implement GraphQL event management; define Event type and createEvent mutation

--- Graphql-Event-Booking/app.js
+++ Graphql-Event-Booking/app.js
@@ -4,17 +4,34 @@ const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 
 const app = express();
+const events = [];
 app.use(bodyParser.json()); //to process incoming json bodies
 app.use(
-  "/graphql",
+  "/graphql", //create route to handle graphQL requests
   graphqlHttp({
+    //handle graphQL operations that a client can perform
     schema: buildSchema(`
+        type Event  {
+                _id:    ID!
+                title:  String!
+                description:    String!
+                price:  Float!
+                date:   String!
+        }
+        
+        input EventInput    {
+                title:  String!
+                description:    String!
+                price:  Float!
+                date:   String! 
+        }
+
         type RootQuery  {
-                events: [String!]!
+                events: [Event!]!  
         }
 
         type RootMutation   {
-                createEvent(name:   String) : String
+                createEvent(eventInput: EventInput) : Event 
         }
 
         schema  {
@@ -22,20 +39,26 @@ app.use(
                 mutation:   RootMutation
         }
         `),
+    //actual functions that run when someone makes a request
     rootValue: {
+      //returns hard coded list of strings
       events: () => {
-        return [
-          "Night out with the Homies",
-          "Watching Harry Potter",
-          "Get lucky",
-        ];
+        return events;
       },
+      //takes an argument and returns it back
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        return event;
       },
     },
-    graphiql: true,
+    graphiql: true, //enables a built-in interactive GUI at "/graphql"
   })
 );
 app.get("/", (req, res, next) => {
