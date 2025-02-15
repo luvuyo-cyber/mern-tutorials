874ce0b768952cf359642966a329ab4e0f2a7b2f
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-02-15 14:36:14 +0200

    Add MongoDB integration and event model; implement event fetching and creation in GraphQL

--- Graphql-Event-Booking/app.js
+++ Graphql-Event-Booking/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
 
 const app = express();
 const events = [];
@@ -43,25 +45,50 @@ app.use(
     rootValue: {
       //returns hard coded list of strings
       events: () => {
-        return events;
+        //return our events
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc, _id: event.id }; //convert our mongodb id to normal string
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       //takes an argument and returns it back
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date),
+        });
+
+        //save to database
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true, //enables a built-in interactive GUI at "/graphql"
   })
 );
-app.get("/", (req, res, next) => {
-  res.send("Hello World");
-});
-app.listen(3000);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.byeyv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
