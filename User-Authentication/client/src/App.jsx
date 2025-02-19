b1d43dc23e596503a09887414213ccb067e8de6f
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-02-20 00:11:00 +0200

    Add Home, Login, and Signup pages; implement routing in App component

--- User-Authentication/client/src/App.jsx
+++ User-Authentication/client/src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Login, Signup } from "./pages";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
