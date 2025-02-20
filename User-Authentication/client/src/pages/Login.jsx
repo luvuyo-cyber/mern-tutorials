8bd343316fdb03168bafa646cf582f4eebefd7bd
luvuyo-cyber <136644800+luvuyo-cyber@users.noreply.github.com>
2025-02-20 13:13:38 +0200

    Enhance styling and layout for Home and Login pages; implement user greeting and logout functionality

--- User-Authentication/client/src/pages/Login.jsx
+++ User-Authentication/client/src/pages/Login.jsx
@@ -9,9 +9,7 @@ const Login = () => {
     email: "",
     password: "",
   });
-
   const { email, password } = inputValue;
-
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setInputValue({
@@ -20,17 +18,14 @@ const Login = () => {
     });
   };
 
-  const handleError = (err) => {
+  const handleError = (err) =>
     toast.error(err, {
       position: "bottom-left",
     });
-  };
-
-  const handleSuccess = (msg) => {
+  const handleSuccess = (msg) =>
     toast.success(msg, {
-      position: "bottom-right",
+      position: "bottom-left",
     });
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -40,13 +35,10 @@ const Login = () => {
         {
           ...inputValue,
         },
-        {
-          withCredentials: true,
-        }
+        { withCredentials: true }
       );
-
+      console.log(data);
       const { success, message } = data;
-
       if (success) {
         handleSuccess(message);
         setTimeout(() => {
@@ -58,45 +50,45 @@ const Login = () => {
     } catch (error) {
       console.log(error);
     }
-
     setInputValue({
       ...inputValue,
       email: "",
       password: "",
     });
   };
+
+  return (
+    <div className="form_container">
+      <h2>Login Account</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={handleOnChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Don't have an account? <Link to={"/signup"}>Signup</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 };
 
-return (
-  <div className="form_container">
-    <h2>Login Account</h2>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          placeholder="Enter your email"
-          onChange={handleOnChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="Enter your password"
-          onChange={handleOnChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
-      <span>
-        Don't have an account? <Link to={"/signup"}>Signup</Link>
-      </span>
-    </form>
-    <ToastContainer />
-  </div>
-);
 export default Login;
